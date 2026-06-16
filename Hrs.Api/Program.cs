248fdd1e958b9c2b57f8b6b3c056b8347498664d
Sunuto <sunutosharma104@gmail.com>
2026-06-17 00:03:34 +0545

    Initial commit

---
+++ Hrs.Api/Program.cs
@@ -0,0 +1,82 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Hrs.Api.Business.BookingBusiness;
+using Hrs.Api.Business.HotelBusiness;
+using Hrs.Api.Business.RoomBusiness;
+using Hrs.Api.Repository.BookingRepository;
+using Hrs.Api.Repository.Data;
+using Hrs.Api.Repository.HotelRepository;
+using Hrs.Api.Repository.RepositoryPattern;
+using Hrs.Api.Repository.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("x-api-version"),
+        new UrlSegmentApiVersionReader()
+    );
+})
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<HrsDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IHotelBusiness, HotelBusiness>();
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IRoomBusiness, RoomBusiness>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IBookingBusiness, BookingBusiness>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    
+    // Enable Swagger middleware and expose one UI endpoint per API version
+    app.UseSwagger();
+
+    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        }
+    });
+    
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
